05578facfa4f4da18e7de5fd0b2f39f13b2aab7d
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2018-09-24 22:25:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QFontDatabase>
+#include <iostream>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
 
+    QFontDatabase database;
+    QTextStream out(stdout); /* out << qstring object here >> endl to print out strings*/
+
+    MainWindow *main_window = new MainWindow;
+
+    /*qDebug() << database.families(); returns all available fonts*/
+    main_window->show();
     return a.exec();
 }
