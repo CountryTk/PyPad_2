2104d711c8f21f7ace150ab842547992f6bff1f5
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2018-09-24 22:31:40 +0300

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,15 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionNew_file_triggered();
+
+    void on_actionCustomize_triggered();
+
+    void on_tabWidget_tabCloseRequested(int index);
+
+    void on_actionQuit_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
