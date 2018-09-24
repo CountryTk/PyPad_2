81c31401da4e77c4f2c5228fdec532d2454dfcd8
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2018-09-24 22:27:58 +0300

    Add files via upload

---
+++ numberbar.h
@@ -0,0 +1,17 @@
+#ifndef NUMBERBAR_H
+#define NUMBERBAR_H
+
+#include <QWidget>
+
+class NumberBar : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit NumberBar(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // NUMBERBAR_H
