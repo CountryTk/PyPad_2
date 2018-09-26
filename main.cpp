f706b3db982e96bb6dc097df006febc66faa46fd
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2018-09-26 07:58:42 +0300

    Dark theme

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <QFontDatabase>
 #include <iostream>
 #include <QDebug>
+#include <QPalette>
+#include <QColor>
 
 int main(int argc, char *argv[])
 {
@@ -11,9 +13,24 @@ int main(int argc, char *argv[])
     QFontDatabase database;
     QTextStream out(stdout); /* out << qstring object here >> endl to print out strings*/
 
+    QPalette *palette = new QPalette;
+
     MainWindow *main_window = new MainWindow;
 
-    /*qDebug() << database.families(); returns all available fonts*/
+   // qDebug() << database.families(); //returns all available fonts
+    palette->setColor(QPalette::Window, QColor("#303030"));
+    palette->setColor(QPalette::WindowText, QColor("#FFFFFF"));
+    palette->setColor(QPalette::Base, QColor("#303030"));
+    palette->setColor(QPalette::AlternateBase, QColor("#FFFFFF"));
+    palette->setColor(QPalette::ToolTipBase, QColor("#FFFFFF"));
+    palette->setColor(QPalette::Text, QColor("#e3e6fc"));
+    palette->setColor(QPalette::Button, QColor("#353535"));
+    palette->setColor(QPalette::ButtonText, QColor("#FFFFFF"));
+    palette->setColor(QPalette::Highlight, QColor("#4DD2FF"));
+    palette->setColor(QPalette::HighlightedText, QColor("#000000"));
+    a.setStyle("Fusion");
+    a.setPalette(*palette);
+
     main_window->show();
     return a.exec();
 }
