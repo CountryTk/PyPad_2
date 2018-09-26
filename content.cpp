14faad2aa88dd4747cf4cb4356f38f7378bc6ffd
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2018-09-26 07:57:13 +0300

    Syntax highlighting + better font

--- content.cpp
+++ content.cpp
@@ -2,6 +2,10 @@
 #include <QPlainTextEdit> //replace this with PlainTextEdit later, a class made by me with auto completion
 #include <QHBoxLayout>
 #include "numberbar.h"
+#include <QFont>
+#include <highlighter.h>
+#include <QFileInfo>
+#include <QDebug>
 
 Content::Content(QWidget *parent, QString text , QString filename, QString basename) : QWidget(parent) {
 
@@ -10,15 +14,27 @@ Content::Content(QWidget *parent, QString text , QString filename, QString basen
     baseName = basename;
 
     NumberBar *numbers = new NumberBar;
-
+    QFont *font = new QFont;
     QHBoxLayout *layout = new QHBoxLayout;
-
     QPlainTextEdit *editor = new QPlainTextEdit;
+    QString filenames = QFileInfo(fileName).completeSuffix();
+
+    if (filenames == "py") {
+        highlighter *syntax = new highlighter(editor->document(), "py");
+
+    } else if (filenames =="cpp" || filenames == "h") {
+         highlighter *syntax = new highlighter(editor->document(), "cpp");
+    }
+
+
+    font->setFamily("Consolas");
+    font->setPointSize(12);
 
     layout->addWidget(numbers);
     layout->addWidget(editor);
     editor->setPlainText(text);
 
+    this->setFont(*font);
     this->setLayout(layout);
 
 }
