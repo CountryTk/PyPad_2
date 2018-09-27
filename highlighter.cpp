2c53d228dd47861f2ed975c4b2c1391de7936414
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2018-09-27 19:14:07 +0300

    Updated rulesets

--- highlighter.cpp
+++ highlighter.cpp
@@ -1,6 +1,8 @@
 #include "highlighter.h"
 #include <QRegularExpression>
 #include <QDebug>
+#include <iostream>
+#include <QDebug>
 
 highlighter::highlighter(QTextDocument *editor_file, QString extension)
     : QSyntaxHighlighter(editor_file) {
@@ -15,8 +17,18 @@ highlighter::highlighter(QTextDocument *editor_file, QString extension)
        QString magic_format;
        QString quotation_pattern2;
        QString integer;
-       if (file_extension == "py") {
 
+       QColor keyword_color;
+       QColor class_color;
+       QColor quotation_color;
+       QColor function_color;
+       QColor single_line_comment_color;
+       QColor decorator_color;
+       QColor magic_color;
+       QColor quotation2_color;
+
+       if (file_extension == "py") {
+              //patterns
               class_pattern = "\\bQ[A-Za-z]+\\b";
               quotation_pattern = "\".*\"";
               function_pattern = "[A-Za-z0-9_]+(?=\\()";
@@ -25,6 +37,15 @@ highlighter::highlighter(QTextDocument *editor_file, QString extension)
               magic_format = "\\__[^']*\\__";
               quotation_pattern2 = "\"[^\"]*\"";
               integer = "[-+]?[0-9]+";
+              //colors
+              keyword_color = "#F6DC74";
+              class_color = "#74F6C3";
+              quotation_color = "#63ff00";
+              function_color = "#FF6666";
+              single_line_comment_color = "#6B6E6C";
+              decorator_color = "#FF00E7";
+              magic_color = "#FF6666";
+              quotation2_color = quotation_color;
 
               QStringList keywordPatterns;
 
@@ -32,7 +53,7 @@ highlighter::highlighter(QTextDocument *editor_file, QString extension)
                       "continue" << "def" << "del" << "elif" <<"else" << "except" << "finally" << "for" <<"from" <<"global"<<
                       "if" <<"import" << "in" << "is" << "lambda" << "nonlocal" << "not" << "or"<< "pass"<< "raise" << "return" <<
                       "try" << "while" << "with" << "yield";
-              keywordFormat.setForeground(Qt::darkBlue);
+              keywordFormat.setForeground(keyword_color);
               keywordFormat.setFontWeight(QFont::Bold);
 
               foreach (const QString &pattern, keywordPatterns) {
@@ -42,15 +63,18 @@ highlighter::highlighter(QTextDocument *editor_file, QString extension)
               }
 
        } else if (file_extension == "cpp") {
-
+           //patterns
            class_pattern = "\\bQ[A-Za-z]+\\b";
            quotation_pattern = "\".*\"";
            function_pattern = "\\b[A-Za-z0-9_]+(?=\\()";
            single_line_comment = "//[^\n]*";
-           decorator_format = "@[^\n]*";
-           magic_format = "\\__[^']*\\__";
-           quotation_pattern2 = "\"[^\"]*\"";
            integer = "[-+]?[0-9]+";
+           //colors
+           keyword_color = "#0099FF";
+           class_color = "#548A9C";
+           quotation_color = "#63ff00";
+           function_color = "#FF9500";
+           single_line_comment_color = "6B6E6C";
 
            QStringList keywordPatterns;
            keywordPatterns << "\\bchar\\b" << "\\bclass\\b" << "\\bconst\\b"
@@ -63,7 +87,7 @@ highlighter::highlighter(QTextDocument *editor_file, QString extension)
                            << "\\btemplate\\b" << "\\btypedef\\b" << "\\btypename\\b"
                            << "\\bunion\\b" << "\\bunsigned\\b" << "\\bvirtual\\b"
                            << "\\bvoid\\b" << "\\bvolatile\\b" << "\\bbool\\b";
-           keywordFormat.setForeground(Qt::darkBlue);
+           keywordFormat.setForeground(keyword_color);
            keywordFormat.setFontWeight(QFont::Bold);
 
            foreach (const QString &pattern, keywordPatterns) {
@@ -74,25 +98,47 @@ highlighter::highlighter(QTextDocument *editor_file, QString extension)
        }
 
        classFormat.setFontWeight(QFont::Bold);
-       classFormat.setForeground(Qt::darkMagenta);
+       classFormat.setForeground(class_color);
        rule.pattern = QRegularExpression(class_pattern);
        rule.format = classFormat;
        highlightingRules.append(rule);
 
-       quotationFormat.setForeground(Qt::darkGreen);
-       rule.pattern = QRegularExpression(quotation_pattern);
-       rule.format = quotationFormat;
-       highlightingRules.append(rule);
-
        functionFormat.setFontItalic(true);
-       functionFormat.setForeground(Qt::blue);
+       functionFormat.setForeground(function_color);
        rule.pattern = QRegularExpression(function_pattern);
        rule.format = functionFormat;
        highlightingRules.append(rule);
 
-       singleLineCommentFormat.setForeground(Qt::red);
-       rule.pattern = QRegularExpression(single_line_comment);
-       rule.format = singleLineCommentFormat;
+       if (magic_format != "") { // If this exists, we are dealing with a python file
+
+           magicFormat.setFontItalic(true);
+           magicFormat.setForeground(magic_color);
+           rule.pattern = QRegularExpression(magic_format);
+           rule.format = magicFormat;
+           highlightingRules.append(rule);
+
+           decoratorFormat.setForeground(decorator_color);
+           rule.pattern = QRegularExpression(decorator_format);
+           rule.format = decoratorFormat;
+           highlightingRules.append(rule);
+
+           quotation2Format.setForeground(quotation2_color);
+           rule.pattern = QRegularExpression(quotation_pattern2);
+           rule.format = quotation2Format;
+           highlightingRules.append(rule);
+
+           singleLineCommentFormat.setForeground(single_line_comment_color);
+           rule.pattern = QRegularExpression(single_line_comment);
+           rule.format = singleLineCommentFormat;
+           highlightingRules.append(rule);
+           std::cout << "test" << std::endl;
+           qDebug() << single_line_comment;
+
+       }
+
+       quotationFormat.setForeground(Qt::darkGreen);
+       rule.pattern = QRegularExpression(quotation_pattern);
+       rule.format = quotationFormat;
        highlightingRules.append(rule);
 
        multiLineCommentFormat.setForeground(Qt::red);
