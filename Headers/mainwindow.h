521713094d7a4004463fe975d57a9080bacfca06
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2018-10-11 16:31:33 +0300

    New functions

--- Headers/mainwindow.h
+++ Headers/mainwindow.h
@@ -41,10 +41,17 @@ private slots:
 
     void open_file_from_menu(QModelIndex signal);
 
+    void open_file_universal(QString file_name);
+
+    bool open_project();
+
+    //void create_project();
+
+    void on_actionOpen_project_triggered();
+
 private:
     Ui::MainWindow *ui;
     Tabs *tab_class;
-    Directory *dir;
 };
 
 #endif // MAINWINDOW_H
