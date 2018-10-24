b2fde0ced102753d282088ce84736631861fdfde
Fuchsiaff <31344416+Fuchsiaff@users.noreply.github.com>
2018-10-24 19:33:30 +0300

    Implemented a search function

--- Headers/plaintextedit.h
+++ Headers/plaintextedit.h
@@ -1,16 +1,23 @@
 #ifndef PLAINTEXTEDIT_H
 #define PLAINTEXTEDIT_H
 #include <QPlainTextEdit>
+#include <find_all.h>
 
 class PlainTextEdit : public QPlainTextEdit {
 
 public:
     PlainTextEdit(QWidget *parent = nullptr);
+
+    QWidget *parent;
     QFont *font;
     void move_cursor_back();
     int replace_tabs;
+    int num = 0;
     QList<int> key_list;
-    QTextCursor cursor;
+    QString string_to_find;
+    QList<int> result_list;
+    bool match_found;
+    find_all *searcher = new find_all(this);
     int pos;
 
 protected:
